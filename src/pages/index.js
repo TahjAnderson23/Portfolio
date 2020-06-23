b0242c043f72451d9a93cd0be2d02cea6795cb5c
Tahj Anderson <tahjanderson@Tahjs-MacBook-Pro.local>
2020-06-22 23:44:21 -0400

    Second push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import {Link} from 'gatsby'
 
-export default function Home() {
-  return <div>Hello world!</div>
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return(
+    <Layout>
+      <h1>Hello</h1>
+      <h2>I am Tahj, a front end developer, living in Georgia</h2>
+
+      <p>Need a developer?<Link to="/contact">Contact me</Link></p>
+    </Layout>
+  )
+
 }
+
+export default IndexPage 
