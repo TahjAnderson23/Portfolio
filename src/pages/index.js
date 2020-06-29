b77b46a5aae62641470595af760add7a24fde4cd
Tahj Anderson <tahjanderson@Tahjs-MacBook-Pro.local>
2020-06-29 00:28:01 -0400

    4th push

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,22 @@ import Layout from '../components/layout'
 import Img from "gatsby-image"
 
 const IndexPage = () => {
-  // const query = useStaticQuery(graphql `
-  //   query{
-  //     fileName: file(relativePath: {eq: "images/IMG_1797.JPG"}) {
-  //       childImageSharp{
-  //         fluid(maxWidth: 400, maxHeight: 250) {
-  //           ...GatsbyImageSharpFluid
-  //         }
-  //       }
-  //     }
-  //   }  
-  // `)
+  const query = useStaticQuery(graphql `
+    query{
+      fileName: file(relativePath: {eq: "images/IMG_1797.JPG"}) {
+        childImageSharp{
+          fluid(maxWidth: 400, maxHeight: 250) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }  
+  `)
   return(
     <Layout>
       <h1>Hello!</h1>
       <h2>I am Tahj, an aspiring front end developer, living in Georgia</h2>
-      {/* <Img fluid={query.fileName.childImageSharp.fluid} alt="@"/> */}
+      <Img fluid={query.fileName.childImageSharp.fluid} alt="@"/>
       <p>I am a sophmore Computer science major at Clemson University (go tigs). </p>
       <p>Need a developer?<Link to="/contact">Contact me</Link></p>
     </Layout>
